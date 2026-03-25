96145613deeb7c4864f82f05c8085645788c13b2
Joel-Ez <joelez.3d@gmail.com>
2026-03-25 01:44:36 +0100

    change the button

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity  } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 
 const ProductCard = ({titel, Description, image, onPress, price}) => {
@@ -7,23 +7,23 @@ const ProductCard = ({titel, Description, image, onPress, price}) => {
     <View style={styles.card}>
       <Text style={styles.title}>{titel}</Text>
       <Image source={image} style={styles.image} />
-      <Text style={styles.price}>{price}</Text>
-      <Text style={styles.description}>{Description}</Text>
-      <Button
-        title='Zie meer' 
+      <Text style={styles.price}>€ {price}</Text>
+      <Text style={styles.description} numberOfLines={5}>{Description}</Text>
+      <TouchableOpacity style={styles.btn}
         onPress={() => navigation.navigate("Product", {
           titel,
           Description,
           price,
           image
-        })}/>
+        })}
+      ><Text style={styles.btnText}>Zie meer</Text></TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    width: 200,
+    width: 'auto',
     backgroundColor: '#fff',
     margin: 10,
     padding: 10,
@@ -32,17 +32,30 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
   },
+  image: {
+  width: '100%',
+  height: 200,
+  borderRadius: 10,
+  marginTop: 10,
+},
   price: {
     textAlign: 'right',
     paddingRight: 10,
     paddingBottom: 10,
   },
-  image: {
-  width: '100%',
-  height: 150,
-  borderRadius: 10,
-  marginTop: 10,
-},
+  description: {
+    marginBottom: 20,
+  },
+  btn: {
+    backgroundColor:'#249CED',
+    borderRadius: 8,
+  },
+  btnText: {
+    color:'#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 10,
+  },
 });
 
 export default ProductCard;
